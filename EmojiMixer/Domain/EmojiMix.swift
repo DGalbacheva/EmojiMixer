45e9f8e5bd4b122546487992799205315c73c100
DGalbacheva <dglbacheva@gmail.com>
2024-11-06 09:25:10 +0300

    Added emojis and backgroundColor in EmojiMix

--- EmojiMixer/Domain/EmojiMix.swift
+++ EmojiMixer/Domain/EmojiMix.swift
@@ -2,7 +2,12 @@
 //  EmojiMix.swift
 //  EmojiMixer
 //
-//  Created by MacBook Pro 15 on 06.11.2024.
+//  Created by Doroteya Galbacheva on 06.11.2024.
 //
 
-import Foundation
+import UIKit
+
+struct EmojiMix {
+    let emojis: String
+    let backgroundColor: UIColor
+}
