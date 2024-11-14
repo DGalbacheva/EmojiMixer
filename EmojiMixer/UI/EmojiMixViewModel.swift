3530eee21cfc157213c720a6811e7359262ff0a7
DGalbacheva <dglbacheva@gmail.com>
2024-11-14 13:01:20 +0300

    Created and added ViewModel - EmojiMixViewModel

--- EmojiMixer/UI/EmojiMixViewModel.swift
+++ EmojiMixer/UI/EmojiMixViewModel.swift
@@ -5,4 +5,28 @@
 //  Created by MacBook Pro 15 on 14.11.2024.
 //
 
-import Foundation
+import UIKit
+
+final class EmojiMixViewModel: Identifiable {
+    let id: String
+    private var emojis: String
+    private var backgroundColor: UIColor
+    
+    init(id: String, emojis: String, backgroundColor: UIColor) {
+        self.id = id
+        self.emojis = emojis
+        self.backgroundColor = backgroundColor
+    }
+    
+    var emojiBinding: Binding<String>? {
+        didSet {
+            emojiBinding?(emojis)
+        }
+    }
+    
+    var backgroundColorBinding: Binding<UIColor>? {
+        didSet {
+            backgroundColorBinding?(backgroundColor)
+        }
+    }
+}
