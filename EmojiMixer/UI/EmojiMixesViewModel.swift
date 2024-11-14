d3847999190b7d7173b5a03c9288dd045f520f65
DGalbacheva <dglbacheva@gmail.com>
2024-11-14 13:14:55 +0300

    Added class EmojiMixesViewModel

--- EmojiMixer/UI/EmojiMixesViewModel.swift
+++ EmojiMixer/UI/EmojiMixesViewModel.swift
@@ -2,7 +2,62 @@
 //  EmojiMixesViewModel.swift
 //  EmojiMixer
 //
-//  Created by MacBook Pro 15 on 14.11.2024.
+//  Created by Doroteya Galbacheva on 14.11.2024.
 //
 
-import Foundation
+import UIKit
+
+final class EmojiMixesViewModel {
+
+    private let emojiMixStore: EmojiMixStore
+    private let emojiMixFactory: EmojiMixFactory
+    private let uiColorMarshalling = UIColorMarshalling()
+
+    private(set) var emojiMixes: [EmojiMixViewModel] = [] {
+        didSet {
+            emojiMixesBinding?(emojiMixes)
+        }
+    }
+    
+    var emojiMixesBinding: Binding<[EmojiMixViewModel]>?
+    
+    convenience init() {
+        let emojiMixStore = try! EmojiMixStore(
+            context: (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        )
+        self.init(emojiMixStore: emojiMixStore, emojiMixFactory: EmojiMixFactory())
+    }
+
+    init(emojiMixStore: EmojiMixStore, emojiMixFactory: EmojiMixFactory) {
+        self.emojiMixStore = emojiMixStore
+        self.emojiMixFactory = emojiMixFactory
+        emojiMixStore.delegate = self
+        emojiMixes = getEmojiMixesFromStore()
+    }
+
+    func addEmojiMixTapped() {
+        let newMix = emojiMixFactory.makeNewMix()
+        try! emojiMixStore.addNewEmojiMix(newMix.emojies, color: newMix.backgroundColor)
+    }
+
+    func deleteAll() {
+        try! emojiMixStore.deleteAll()
+    }
+
+    private func getEmojiMixesFromStore() -> [EmojiMixViewModel] {
+        return emojiMixStore.emojiMixes.map {
+            EmojiMixViewModel(
+                id: $0.objectID.uriRepresentation().absoluteString,
+                emojis: $0.emojies ?? "",
+                backgroundColor: uiColorMarshalling.color(from: $0.colorHex ?? "")
+            )
+        }
+    }
+}
+
+extension EmojiMixesViewModel: EmojiMixStoreDelegate {
+    func storeDidUpdate(_ store: EmojiMixStore) {
+        emojiMixes = getEmojiMixesFromStore()
+    }
+}
+
