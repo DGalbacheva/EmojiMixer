4ef74dc5fa777d9cec7c62460c3f37887acc81e5
DGalbacheva <dglbacheva@gmail.com>
2024-11-15 12:04:51 +0300

    Cell cornerRadius set to 10.00

--- EmojiMixer/UI/EmojiMixCollectionViewCell.swift
+++ EmojiMixer/UI/EmojiMixCollectionViewCell.swift
@@ -31,10 +31,13 @@ final class EmojiMixCollectionViewCell: UICollectionViewCell {
             titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
             titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
         ])
+        
+        contentView.layer.cornerRadius = 10.0
+        contentView.layer.masksToBounds = true
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     override func prepareForReuse() {
